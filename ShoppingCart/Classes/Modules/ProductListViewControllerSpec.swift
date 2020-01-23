de2ffefca75c5c32677b18b056243e1604f7dceb
Jakub Sowa <sowa.jakub@gmail.com>
2020-01-23 16:20:01 +0100

    Added ProductListViewController

---
+++ ShoppingCart/Classes/Modules/ProductListViewControllerSpec.swift
@@ -0,0 +1,17 @@
+import Foundation
+import Quick
+import Nimble
+
+@testable import ShoppingCart
+
+class ProductListViewControllerSpec: QuickSpec {
+    override func spec() {
+        describe("ProductListViewController") {
+            var sut: ProductListViewController!
+        
+            it("should be true") {
+                
+            }
+        }
+    }
+}
